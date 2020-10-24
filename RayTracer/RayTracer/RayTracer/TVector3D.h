41879dc6b18e3cbfa5387e067602d37d4a38acd4
Jason <jasoncoltier@gmail.com>
2020-10-24 23:48:17 +0200

    vector almost finished

--- RayTracer/RayTracer/RayTracer/TVector3D.h
+++ RayTracer/RayTracer/RayTracer/TVector3D.h
@@ -2,47 +2,189 @@
 #include <iostream>
 #include <ostream>
 #include "TPoint.h";
+#include <math.h>;
 
 template <typename T>
 class TVector3D
 {
-	static_assert(std::is_pod<T>::value, "not a number !");
+    static_assert(std::is_pod<T>::value, "not a number !");
 
 private:
-	//coordonnées de mon vecteur
-	T delta_x;
-	T delta_y;
-	T delta_z;
+    //coordonnï¿½es de mon vecteur
+    T delta_x;
+public:
+
+
+private:
+    T delta_y;
+    T delta_z;
 
 public:
-	TVector3D(const T& x,const T& y,const T& z) : delta_x(x),delta_y(y),delta_z(z)
-	{
-		std::cout << "new vector created : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
-	}
-	TVector3D(TPoint<T>& point1,TPoint<T>& point2)
-	{
-		delta_x = point2.GetX() - point1.GetX();
-		delta_y = point2.GetY() - point1.GetY();
-		delta_z = point2.GetZ() - point1.GetZ();
-		std::cout << "new vector created by points : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
-	}
-	TVector3D(TVector3D& ref_vector) : delta_x(ref_vector.delta_x), delta_y(ref_vector.delta_y), delta_z(ref_vector.delta_z)
-	{
-		std::cout << "new vector created by copy : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
-	}
+    TVector3D() : delta_x(0), delta_y(0), delta_z(0)
+    {
+        std::cout << "new vector created : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
+    }
 
-	T GetX() { return delta_x; }
-	T GetY() { return delta_y; }
-	T GetZ() { return delta_z; }
+    TVector3D(const T& x, const T& y, const T& z) : delta_x(x), delta_y(y), delta_z(z)
+    {
+        std::cout << "new vector created : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
+    }
 
+    TVector3D(TPoint<T>& point1, TPoint<T>& point2)
+    {
+        delta_x = point2.GetX() - point1.GetX();
+        delta_y = point2.GetY() - point1.GetY();
+        delta_z = point2.GetZ() - point1.GetZ();
+        std::cout << "new vector created by points : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
+    }
 
-	void operator+(int add) { delta_x += add; delta_y += add; delta_z += add; };
-	void operator-(int sub) { delta_x -= sub; delta_y -= sub; delta_z -= sub; };
+    TVector3D(TVector3D& ref_vector) : delta_x(ref_vector.delta_x), delta_y(ref_vector.delta_y),
+                                       delta_z(ref_vector.delta_z)
+    {
+        std::cout << "new vector created by copy : " << delta_x << " " << delta_y << " " << delta_z << std::endl;
+    }
 
+    ~TVector3D()
+    {
+        std::cout << " ~ ";
+        // std::cout<<delta_x<<" " <<delta_y<<" " <<delta_z<<" ";
+    }
+
+    /*
+     * GETTERS ET SETTERS
+     */
+    T GetX() { return delta_x; }
+    T GetY() { return delta_y; }
+    T GetZ() { return delta_z; }
+    void set_delta_x(const T& delta_x) { this->delta_x = delta_x; }
+    void set_delta_y(const T& delta_y) { this->delta_y = delta_y; }
+    void set_delta_z(const T& delta_z) { this->delta_z = delta_z; }
+
+    /*
+     * OPERATIONS AVEC =
+     */
+    void operator+=(T add)
+    {
+        delta_x += add;
+        delta_y += add;
+        delta_z += add;
+    }
+
+    void operator-=(T sub)
+    {
+        delta_x -= sub;
+        delta_y -= sub;
+        delta_z -= sub;
+    }
+
+    void operator*=(T mul)
+    {
+        delta_x *= mul;
+        delta_y *= mul;
+        delta_z *= mul;
+    }
+
+    void operator/=(T div)
+    {
+        delta_x /= div;
+        delta_y /= div;
+        delta_z /= div;
+    }
+
+    /*
+     * OPERTATIONS SIMPLES
+     */
+    TVector3D operator+(T add)
+    {
+        TVector3D<T> result;
+        result.set_delta_x(this->delta_x + add);
+        result.set_delta_y(this->delta_y + add);
+        result.set_delta_z(this->delta_z + add);
+        return result;
+    }
+
+    TVector3D operator-(T sub)
+    {
+        TVector3D<T> result;
+        result.set_delta_x(this->delta_x - sub);
+        result.set_delta_y(this->delta_y - sub);
+        result.set_delta_z(this->delta_z - sub);
+        return result;
+    }
+
+    TVector3D operator*(T mul)
+    {
+        TVector3D<T> result;
+        result.set_delta_x(this->delta_x * mul);
+        result.set_delta_y(this->delta_y * mul);
+        result.set_delta_z(this->delta_z * mul);
+        return result;
+    }
+
+    TVector3D operator/(T div)
+    {
+        TVector3D<T> result;
+        result.set_delta_x(this->delta_x / div);
+        result.set_delta_y(this->delta_y / div);
+        result.set_delta_z(this->delta_z / div);
+        return result;
+    }
+
+    /*
+     * OPERATIONS COMPEXES
+     */
+
+    void operator*=(TVector3D& vector) //cross product, produit vectoriel
+    {
+        T tmpX = delta_x;
+        T tmpY = delta_y;
+        T tmpZ = delta_z;
+
+        delta_x = tmpY * vector.GetZ() - tmpZ * vector.GetY();
+        delta_y = tmpZ * vector.GetX() - tmpX * vector.GetZ();
+        delta_z = tmpX * vector.GetY() - tmpY * vector.GetX();
+    }
+
+    TVector3D operator*(TVector3D& vector) //cross product, produit vectoriel
+    {
+        TVector3D<T> result;
+
+        result.set_delta_x(this->delta_y * vector.GetZ() - this->delta_z * vector.GetY());
+        result.set_delta_y(this->delta_z * vector.GetX() - this->delta_x * vector.GetZ());
+        result.set_delta_z(this->delta_x * vector.GetY() - this->delta_y * vector.GetX());
+        return result;
+    }
+
+    T Magnitude() // ||a|| ou length
+    {
+    	return sqrt(pow(delta_x,2) + pow(delta_y,2) + pow(delta_z,2));
+    }
+
+    TVector3D Normalize()
+    {
+        TVector3D<T> result = *this;
+        float mag = result.Magnitude();
+        result /= mag;
+        return result;
+    }
+
+    TVector3D Invert()
+    {
+        TVector3D<T> result = *this;
+        result *= -1;
+        return result;
+    }
+    
 };
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, TVector3D<T>& vector) {
-	return os << vector.GetX() << " " << vector.GetY() << " " << vector.GetZ();
-};
+T Dot(TVector3D<T> &v1, TVector3D<T> &v2)
+{
+    return v1.GetX()*v2.GetX() + v1.GetY()*v2.GetY() +v1.GetZ()*v2.GetZ();
+}
 
+template <typename T>
+std::ostream& operator<<(std::ostream& os, TVector3D<T>& vector)
+{
+    return os << "(" << vector.GetX() << ", " << vector.GetY() << ", " << vector.GetZ() << ")";
+};
--- RayTracer/RayTracer/RayTracer/main.cpp
+++ RayTracer/RayTracer/RayTracer/main.cpp
@@ -1,4 +1,6 @@
+#include <complex.h>
 #include <iostream>
+#include <iso646.h>
 #include <ostream>
 #include "TPoint.h"
 #include "TVector3D.h"
@@ -17,12 +19,50 @@ int main()
 	TVector3D<int> vector1(2, 5, 3);
 	TVector3D<int> vector2(point1,point4);           
 	TVector3D<int> vector3(vector1);           
+	TVector3D<int> vector4(3,-5,4);           
+	TVector3D<int> vector5(2,6,5);           
+	TVector3D<float> vector6(5,5,5);            
 
 	std::cout << std::endl; 
-	std::cout << "starting operations : + 2" << std::endl;
-	vector1 + 2;
+	std::cout << "starting operations : vector1 + 2" << std::endl;
+	vector1 += 2;
 	std::cout << vector1  <<std::endl;
-	vector1 - 1;
+	std::cout << "vector2 =  vector1 + 2" << std::endl;
+	vector2 = vector1 + 2;
+	std::cout << vector2  <<std::endl;
+	
+	std::cout << "- 1" << std::endl;
+	vector1 -= 1;
 	std::cout << vector1 << std::endl;
+	std::cout << "* 3" << std::endl;
+	vector1 *= 3;
+	std::cout << vector1 << std::endl;
+	std::cout << "/ 2" << std::endl;
+	vector1 /= 2;
+	std::cout << "vector 1 : " << vector1 << std::endl;
+	std::cout << "vector4 * vector5" << std::endl;
+	vector3 = vector4 * vector5;
+	std::cout << vector3 << std::endl;
+	
+	std::cout << "magnitude de v1 : " << std::endl;
+	auto mag =  vector1.Magnitude();
+	std::cout << mag << std::endl;
+
+	std::cout << "normalisation de v6 : " << std::endl;
+	TVector3D<float> norm;
+	norm = vector6.Normalize();
+	std::cout <<"normalisation : "<< norm << std::endl;
+
+	std::cout << "dot de v1 et v2: " << std::endl;
+	std::cout << "v1: " <<vector1<< std::endl;
+	std::cout << "v2: " <<vector2<< std::endl;
+	auto dot =  Dot(vector1,vector2);	
+	std::cout << dot << std::endl;
+
+	std::cout << "invert de v6 : " << vector6<< std::endl;
+	TVector3D<float> invert;
+	invert = vector6.Invert();
+	std::cout << invert << std::endl;
+
 	
 }
